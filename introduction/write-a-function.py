0760fba259a4b38c50423ded5708bd92c76187ea
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-07-07 07:33:24 +0700

    Solution for write a function https://www.hackerrank.com/challenges/write-a-function/problem

---
+++ introduction/write-a-function.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 == 0:
+            leap = True
+            return leap
+        elif year % 100 == 0 and not year % 400 == 0:
+            return leap
+        
+        else:
+            leap = True
+            return leap
+
+    if not year % 4 == 0:
+        return leap
+
