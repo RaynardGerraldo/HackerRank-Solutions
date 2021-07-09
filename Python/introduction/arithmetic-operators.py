c51ff94353c65073f2892eabe0919abd5d502e91
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-07-10 00:10:37 +0700

    My solution for https://www.hackerrank.com/challenges/python-arithmetic-operators/problem

---
+++ Python/introduction/arithmetic-operators.py
@@ -0,0 +1,10 @@
+def arith_op(a,b):
+    addition = a + b
+    subtract = a - b 
+    multiply = a * b
+        
+    print(addition)
+    print(subtract)
+    print(multiply)
+        
+arith_op(int(input()),int(input()))    
